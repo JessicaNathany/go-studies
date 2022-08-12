2d88918c48d039e36ea5dec15bcffa9d8f28f5b2
Jéssica Nathany <jessicanathany.web@gmail.com>
2022-08-12 08:58:32 -0300

    new file:   exercises-example/salary-calculate.go

---
+++ exercises-example/salary-calculate.go
@@ -0,0 +1,21 @@
+package main
+
+// Program: Calculate the salary the employers
+
+type Person struct {
+	Name     string
+	Position string
+	Wage     float64
+}
+
+func SalaryCalculate() {
+	// calculate salary here
+}
+
+func DiscountPercentageSalry() {
+	// calculate discount salary
+}
+
+func main() {
+	// print the result
+}
