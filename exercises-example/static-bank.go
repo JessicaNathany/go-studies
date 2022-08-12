4c4683c5572fec7404161acbc983b7024768da89
Jéssica Nathany <jessicanathany.web@gmail.com>
2022-08-12 09:31:42 -0300

    new file:   exercises-example/static-bank.go

---
+++ exercises-example/static-bank.go
@@ -0,0 +1,40 @@
+package main
+
+type Person struct {
+	Name  string
+	Email string
+}
+
+type Account struct {
+	AccountNumber string
+	Balance       float64
+	Bank          string
+}
+
+func WithdrawMoney(){
+	// option 2
+}
+
+func SeeBalance(){
+	// option 1
+}
+
+func TransferMoney(){
+	// option 3
+}
+
+func DepositMoney(){
+	// option 4
+}
+
+
+func main() {
+	// menu options the bank
+
+	/*
+	  1. See balance
+	  2. withdraw money
+	  3. Transfer
+	  4. Deposit
+	*/
+}
