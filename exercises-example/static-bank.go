a6cb53a8e9a7e834030a486c64600941dc169715
Jéssica Nathany <jessicanathany.web@gmail.com>
2022-08-12 17:54:01 -0300

    modified:   exercises-example/static-bank.go

--- exercises-example/static-bank.go
+++ exercises-example/static-bank.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Person struct {
 	Name  string
 	Email string
@@ -11,30 +13,42 @@ type Account struct {
 	Bank          string
 }
 
-func WithdrawMoney(){
+func WithdrawMoney(option int) {
 	// option 2
 }
 
-func SeeBalance(){
+func SeeBalance(option int) {
 	// option 1
 }
 
-func TransferMoney(){
+func TransferMoney(option int) {
 	// option 3
 }
 
-func DepositMoney(){
+func DepositMoney(option int) {
 	// option 4
 }
 
+func ReceiveOptions(options int) {
+
+	switch options {
+	case 1:
+		SeeBalance(1)
+	case 2:
+		WithdrawMoney(2)
+	case 3:
+		TransferMoney(2)
+	case 4:
+		DepositMoney(2)
+	default:
+		fmt.Println("Not found")
+	}
+}
 
 func main() {
-	// menu options the bank
-
-	/*
-	  1. See balance
-	  2. withdraw money
-	  3. Transfer
-	  4. Deposit
-	*/
+	fmt.Println("What do you search? Enter in a options: ")
+	fmt.Println("1.\tSee balance \n2.\tWithdraw Money \n3.\tTransfer \n4.\tDeposit")
+	var options int
+	fmt.Scanln(&options)
+	ReceiveOptions(options)
 }
