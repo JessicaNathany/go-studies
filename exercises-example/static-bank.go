1bcb8123f42457eac082596680d95950b6678259
Jéssica Nathany <jessicanathany.web@gmail.com>
2022-09-06 09:37:47 -0300

    modified:   exercises-example/static-bank.go

--- exercises-example/static-bank.go
+++ exercises-example/static-bank.go
@@ -13,35 +13,89 @@ type Account struct {
 	Bank          string
 }
 
-func WithdrawMoney(option int) {
-	// option 2
+func SeeBalance(option int) {
+
+	OptionsValidation(option)
+
+	var balance = Account{"3876", 300.02, "Bank Hash Deck"}
+	fmt.Println("\tBalance Information: ")
+	fmt.Println("\tAccount Number: ", balance.AccountNumber)
+	fmt.Println("\tAccount Balance: ", balance.Balance)
+	fmt.Println("\tAccount Bank: ", balance.Bank)
 }
 
-func SeeBalance(option int) {
-	// option 1
+func WithdrawMoney(option int) {
+	OptionsValidation(option)
+
+	fmt.Println("Inform the withdrawal amount $: ")
+
+	var value float64
+	fmt.Scanln(&value)
+
+	var account = Account{"3876", 300.02, "Bank Hash Deck"}
+
+	var withdrawal = account.Balance - value
+	account.Balance = withdrawal
+
+	fmt.Println("\t Withdrawal was successful!  ")
+	fmt.Println("\t Balance Information: ")
+	fmt.Println("\tAccount Number: ", account.AccountNumber)
+	fmt.Println("\tAccount Balance: ", account.Balance)
+	fmt.Println("\tAccount Bank: ", account.Bank)
 }
 
 func TransferMoney(option int) {
-	// option 3
+	OptionsValidation(option)
+
+	var account = Account{"3876", 800.02, "Bank Hash Deck"}
+
+	fmt.Println("Inform destination account number: ")
+	var destinationAccountNumber string
+	fmt.Scanln(&destinationAccountNumber)
+
+	fmt.Println("Inform the value for transfer: ")
+	var transferValue float64
+	fmt.Scanln(&transferValue)
+
+	var value = account.Balance - transferValue
+	account.Balance = value
+
+	fmt.Println("\t Transfer was successful!  ")
+	fmt.Println("\t Account Destination Information: ")
+	fmt.Println("\tAccount Number: ", destinationAccountNumber)
+	fmt.Println("\tAccount Bank: ", "American Digital Bank")
+	fmt.Println("\tAmount transferred: ", transferValue)
+
+	fmt.Println("\t Your balance: ")
+	fmt.Println("\tAccount Number: ", account.AccountNumber)
+	fmt.Println("\tAccount Balance: ", account.Balance)
+	fmt.Println("\tAccount Bank: ", account.Bank)
 }
 
 func DepositMoney(option int) {
-	// option 4
+	OptionsValidation(option)
 }
 
 func ReceiveOptions(options int) {
 
 	switch options {
 	case 1:
-		SeeBalance(1)
+		SeeBalance(options)
 	case 2:
-		WithdrawMoney(2)
+		WithdrawMoney(options)
 	case 3:
-		TransferMoney(2)
+		TransferMoney(options)
 	case 4:
-		DepositMoney(2)
+		DepositMoney(options)
 	default:
-		fmt.Println("Not found")
+		fmt.Println("Options not found")
+	}
+}
+
+func OptionsValidation(options int) {
+
+	if options == 0 {
+		fmt.Println(options, "invalid option!")
 	}
 }
 
